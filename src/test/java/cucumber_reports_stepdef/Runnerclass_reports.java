d29c241425f421a95c09c85a72f4bbbd64c0a3e9
vishnupriya-58 <54269367+vishnupriya-58@users.noreply.github.com>
2019-08-21 14:19:58 +0530

    Update Runnerclass_reports.java

--- src/test/java/cucumber_reports_stepdef/Runnerclass_reports.java
+++ src/test/java/cucumber_reports_stepdef/Runnerclass_reports.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="Feature456",glue="cucumber_reports_stepdef",plugin={"json:target/cucumber.json"})
+@CucumberOptions(plugin={"json:target/cucumber.json"},features="Feature456",glue="cucumber_reports_stepdef")
 public class Runnerclass_reports {
 
 }
